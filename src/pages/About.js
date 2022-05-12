8fe7098d2193b3169a32b802b604afb977ce14f7
unknown <moonahlee@gmail.com>
2022-05-12 21:18:50 +0900

    added serverless

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,9 +11,35 @@ const About = () => {
 
                 <a className="btn btn-primary" href="https://github.com/AlexSegen/react-shopping-cart">
                     <GitHubIcon width={"18px"}/> <span className="ml-2 mr-4">Coupon</span></a>
+                    <button type="button" onclick="callAPI(document.getElementById('fName').value,document.getElementById('lName').value)">Call API</button>
+
+                    
             </div>
         </Layout>
      );
 }
  
+
+// define the callAPI function that takes a first name and last name as parameters
+var callAPI = (firstName,lastName)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("YOUR-API-INVOKE-URL", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
+
 export default About;
